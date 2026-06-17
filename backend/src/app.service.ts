639b8ae6e62fa5f3cc72368e9da955b9fc4354e0
Manisha Gangadevi <manishagangadevi1@gmail.com>
2026-06-17 11:37:49 +0530

    refactor: simplify AppService by removing PrismaClient integration

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,13 +1,8 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  async onModuleInit() {
-    await this.$connect();
-  }
-
-  async onModuleDestroy() {
-    await this.$disconnect();
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
   }
 }
